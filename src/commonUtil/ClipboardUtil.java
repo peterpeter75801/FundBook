dd29f9df5545df25a8352b113260e3263487f34a
peterpeter75801 <peterpeter75801@gmail.com>
2018-10-01 01:18:16 +0800

    2018.09.30 upload

--- src/commonUtil/ClipboardUtil.java
+++ src/commonUtil/ClipboardUtil.java
@@ -1,10 +1,20 @@
 package commonUtil;
 
 import java.awt.Component;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
+import javax.swing.text.JTextComponent;
 
 public class ClipboardUtil {
     
     public static void cut( Component component ) {
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        if( component instanceof JTextComponent ) {
+            String copiedString = ((JTextComponent)component).getText();
+            clipboard.setContents( new StringSelection( copiedString ), null );
+        }
         
     }
     
